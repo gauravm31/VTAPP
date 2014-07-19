3a3a84e73d4332af8b61706419850e44451b1370
Gaurav Mahajan <gaurav@vinsol.com>
2014-07-19 13:35:20 +0530

    modifies files for customer_account_balance. Adds more checks.

--- BasicRuby/customer_account_balance/lib/customer.rb
+++ BasicRuby/customer_account_balance/lib/customer.rb
@@ -1,19 +1,30 @@
 class Customer
-  @@account_no = 0
+  @@account_number = 0
   attr_reader :name, :account_no, :balance
 
   def initialize(name)
     @name = name
-    @account_no = @@account_no += 1
+    @account_no = @@account_number += 1
     @balance = 1000
   end
 
   def deposit(amount)
-    @balance += amount
+    amount < 0 ? puts('Please enter a positive amount.') : @balance += amount
   end
 
   def withdrawal(amount)
-    amount > @balance ? puts('Not enough balance') : @balance -= amount
+    #amount > @balance ? puts('Not enough balance.') : @balance -= amount
+    if amount < 0
+      puts 'Please enter a positive amount.'
+
+    elsif amount > @balance
+      puts 'Not enough funds'
+
+    else
+      @balance -= amount
+
+    end
+
   end
 
 end
