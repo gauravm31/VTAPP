2f2e844934091c61d849de39efe1e2fe328579ad
Gaurav Mahajan <gaurav@vinsol.com>
2014-07-21 18:23:03 +0530

    Modify sum_time to use Time class.

--- BasicRuby/sum_time/lib/time_sum.rb
+++ BasicRuby/sum_time/lib/time_sum.rb
@@ -1,39 +1,36 @@
+require "time"
+
 class TimeSum
 
-  def sum_of_times(time1, time2)
-    if /\d+:\d+:\d+/ !~ time1
+  def sum_of_times(arg1, arg2)
+    if /\d\d?:\d\d?:\d\d?/ !~ arg1
       "time1 is not valid"
-    elsif /\d+:\d+:\d+/ !~ time2
+    elsif /\d\d?:\d\d?:\d\d?/ !~ arg2
       "time2 is not valid"
     else
-      /(?<hours1>\d+):(?<minutes1>\d+):(?<seconds1>\d+)/ =~ time1
-      /(?<hours2>\d+):(?<minutes2>\d+):(?<seconds2>\d+)/ =~ time2
-      if (hours1.to_i >= 24 || minutes1.to_i >= 60 || seconds1.to_i >= 60)
-        "time1 is not valid"
-      elsif (hours2.to_i >= 24 || minutes2.to_i >= 60 || seconds2.to_i >= 60)
-        "time2 is not valid"
-      else
-        sum_seconds = seconds1.to_i + seconds2.to_i
-        sum_minutes = minutes1.to_i + minutes2.to_i
-        sum_hours = hours1.to_i + hours2.to_i
+      time1 = Time.parse(arg1)
+      time2 = Time.parse(arg2)
+      sum_seconds = time1.sec + time2.sec
+      sum_minutes = time1.min + time2.min
+      sum_hours = time1.hour + time2.hour
 
-        if sum_seconds >= 60
-          sum_seconds -= 60
-          sum_minutes +=1
-        end
-
-        if sum_minutes >= 60
-          sum_minutes -= 60
-          sum_hours +=1
-        end
-
-        if sum_hours >= 24
-          sum_hours -= 24
-          "1 day and #{sum_hours}:#{sum_minutes}:#{sum_seconds}"
-        else
-          "#{sum_hours}:#{sum_minutes}:#{sum_seconds}"
-        end
+      if sum_seconds >= 60
+        sum_seconds -= 60
+        sum_minutes +=1
       end
+
+      if sum_minutes >= 60
+        sum_minutes -= 60
+        sum_hours +=1
+      end
+
+      if sum_hours >= 24
+        sum_hours -= 24
+        "1 day and #{sum_hours}:#{sum_minutes}:#{sum_seconds}"
+      else
+        "#{sum_hours}:#{sum_minutes}:#{sum_seconds}"
+      end
+    
     end
   end
 end
