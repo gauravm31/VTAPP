925d4d8fd84df463c2f7586a46709b7dc58c3a90
Gaurav Mahajan <gaurav@vinsol.com>
2014-07-21 16:09:11 +0530

    Modify hash_inject to use appropriate hash names.

--- BasicRuby/hash_inject/lib/array.rb
+++ BasicRuby/hash_inject/lib/array.rb
@@ -2,19 +2,19 @@ class Array
 
   def group_by_length  
 
-    hash = Hash.new { |hash, key| hash[key] = [] }
+    hash_by_length = Hash.new { |hash, key| hash[key] = [] }
     for index in 0...size
-      hash[self[index].to_s.length] << self[index]
+      hash_by_length[self[index].to_s.length] << self[index]
     end
-    hash
+    hash_by_length
   end
 
   def even_odd_hash
-    hash = group_by_length
-    hash1 = Hash.new { |hash, key| hash[key] = [] }
-    hash.inject(0) do |_, (key, value) |
-      key.odd? ? (hash1[:odd] << value) : (hash1[:even] << value)
+    hash_by_length = group_by_length
+    hash_by_even_odd = Hash.new { |hash, key| hash[key] = [] }
+    hash_by_length.inject(0) do |_, (key, value) |
+      key.odd? ? (hash_by_even_odd[:odd] << value) : (hash_by_even_odd[:even] << value)
     end
-    hash1
+    hash_by_even_odd
   end
 end
