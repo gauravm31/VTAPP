136dcc05c7ac1a824506b722140726701818b18b
Gaurav Mahajan <gaurav@vinsol.com>
2014-07-21 12:28:10 +0530

    Modify factorial_ranges to remove indentation issues.

--- BasicRuby/factorial_ranges/lib/fixnum.rb
+++ BasicRuby/factorial_ranges/lib/fixnum.rb
@@ -3,13 +3,11 @@ class Fixnum
   def factorial
 
     if self < 0
-    "Enter a positive number."
+      "Enter a positive number."
     elsif self <= 1
       1
     else
       (2..self).inject(:*)
     end
-
   end
-
 end
