0327346fedd14765f53d118b6028bd92f97d9a9e
Gaurav Mahajan <gaurav@vinsol.com>
2014-07-21 15:46:21 +0530

    Modify lib file to use symbols as hash keys and break even_odd_hash in two separate methods.

--- BasicRuby/hash_inject/lib/array.rb
+++ BasicRuby/hash_inject/lib/array.rb
@@ -1,20 +1,20 @@
 class Array
 
-  def even_odd_hash  
+  def group_by_length  
 
     hash = Hash.new { |hash, key| hash[key] = [] }
     for index in 0...size
       hash[self[index].to_s.length] << self[index]
     end
-
-    hash1 = Hash.new { |hash, key| hash[key] = [] }
-
-    hash.inject(0) do |_, (key, value) |
-
-      key.odd? ? (hash1["odd"] << value) : (hash1["even"] << value)
-    end
-
-    hash1
+    hash
   end
 
+  def even_odd_hash
+    hash = group_by_length
+    hash1 = Hash.new { |hash, key| hash[key] = [] }
+    hash.inject(0) do |_, (key, value) |
+      key.odd? ? (hash1[:odd] << value) : (hash1[:even] << value)
+    end
+    hash1
+  end
 end
