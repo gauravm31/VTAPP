db085bb902d4c964ad7fb54c242b41012cf553e6
Gaurav Mahajan <gaurav@vinsol.com>
2014-07-22 11:58:17 +0530

    Modify pascal_yield to remove issues as discussed.

--- BasicRuby/pascal_yield/lib/pascal_triangle.rb
+++ BasicRuby/pascal_yield/lib/pascal_triangle.rb
@@ -1,23 +1,17 @@
-class PascalTriangle
+class Pascal
 
-  def pascal(number)
+  def Pascal.build(number)
     array = []
 
-    for row in 0..number
-
+    (0..number).each do | row |
       nested_array = []
       nested_array[0] = 1
-
-      for column in 1..row
-        nested_array[column] = array[row -1][column-1] + (array[row - 1][column] || 0)
-
+      (1..row).each do | column |
+        nested_array[column] = array[row - 1][column - 1] + (array[row - 1][column] || 0)
       end
 
       yield(nested_array)
       array[row] = nested_array
-
     end
-
   end
-
 end
