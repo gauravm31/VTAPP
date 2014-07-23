1fac230f87e76d18eb135835fd1acc0113674b12
Gaurav Mahajan <gaurav@vinsol.com>
2014-07-23 15:40:42 +0530

    Use method 'upto' instead of ranges.

--- BasicRuby/pascal_yield/bin/main.rb
+++ BasicRuby/pascal_yield/bin/main.rb
@@ -1,4 +1,4 @@
-require_relative "../lib/pascal_triangle.rb"
+require_relative "../lib/pascal"
 
 Pascal.build(6) do |row|
   row.each { |element| print "#{element} " }
--- BasicRuby/pascal_yield/lib/pascal.rb
+++ BasicRuby/pascal_yield/lib/pascal.rb
@@ -3,10 +3,10 @@ class Pascal
   def Pascal.build(number)
     array = []
 
-    (0..number).each do | row |
+    0.upto(number) do | row |
       nested_array = []
       nested_array[0] = 1
-      (1..row).each do | column |
+      1.upto(row) do | column |
         nested_array[column] = array[row - 1][column - 1] + (array[row - 1][column] || 0)
       end
 
