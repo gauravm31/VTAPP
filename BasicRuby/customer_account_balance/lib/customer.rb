5483a914ca2af3a142d1045a242050ef16b88b73
Gaurav Mahajan <gaurav@vinsol.com>
2014-07-23 18:01:34 +0530

    Remove indentation issues and change '@@customer_number' to '@@number_of_accounts'.

--- BasicRuby/customer_account_balance/lib/customer.rb
+++ BasicRuby/customer_account_balance/lib/customer.rb
@@ -1,30 +1,28 @@
 class Customer
-  @@account_number = 0
+  @@number_of_accounts = 0
   attr_reader :name, :account_no, :balance
 
   def initialize(name)
     @name = name
-    @account_no = @@account_number += 1
+    @account_no = @@number_of_accounts += 1
     @balance = 1000
   end
 
   def deposit(amount)
-    amount < 0 ? puts('Please enter a positive amount.') : @balance += amount
+    if amount < 0
+      'Please enter a positive amount.'
+    else
+      "Balance = #{ @balance += amount }"
+    end
   end
 
   def withdrawal(amount)
-    #amount > @balance ? puts('Not enough balance.') : @balance -= amount
     if amount < 0
-      puts 'Please enter a positive amount.'
-
+      'Please enter a positive amount.'
     elsif amount > @balance
-      puts 'Not enough funds'
-
+      'Not enough funds'
     else
-      @balance -= amount
-
+      "Balance = #{ @balance -= amount }"
     end
-
   end
-
 end
