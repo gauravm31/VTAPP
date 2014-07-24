5b53f3e89f5d42c536d35cb7033ff4d5611994c7
Gaurav Mahajan <gaurav@vinsol.com>
2014-07-24 19:37:57 +0530

    Changed class variable from 'number_of_accounts' to 'number_of_objects'. Modify deposit and withdrawal to return just balance when succesful. Remove repetition. Add a to_s method to `puts` objects.

--- BasicRuby/customer_account_balance/bin/main.rb
+++ BasicRuby/customer_account_balance/bin/main.rb
@@ -3,15 +3,15 @@ require_relative '../lib/customer'
 customer1 = Customer.new('Gaurav')
 customer2 = Customer.new('Dilpreet')
 customer3 = Customer.new('Amit')
-puts "#{ customer1.name } has account number: #{ customer1.account_no } with balance = #{ customer1.balance }"
-puts "#{ customer2.name } has account number: #{ customer2.account_no } with balance = #{ customer2.balance }"
-puts "#{ customer3.name } has account number: #{ customer3.account_no } with balance = #{ customer3.balance }"
+puts customer1
+puts customer2
+puts customer3
 puts 'Amit withdraws 200 rs.'
 puts customer3.withdrawal(200)
 puts 'Gaurav deposits 2200 rs.'
 puts customer1.deposit(2200)
 puts 'Dilpreet withdraws 1500rs.'
 puts customer2.withdrawal(1500)
-puts "#{ customer1.name } has account number: #{ customer1.account_no } with balance = #{ customer1.balance }"
-puts "#{ customer2.name } has account number: #{ customer2.account_no } with balance = #{ customer2.balance }"
-puts "#{ customer3.name } has account number: #{ customer3.account_no } with balance = #{ customer3.balance }"
+puts customer1
+puts customer2
+puts customer3
--- BasicRuby/customer_account_balance/lib/customer.rb
+++ BasicRuby/customer_account_balance/lib/customer.rb
@@ -1,29 +1,34 @@
 class Customer
-  @@number_of_accounts = 0
+  @@number_of_objects = 0
   attr_reader :name, :account_no, :balance
+  MESSAGE = 'Please enter a positive amount.'
 
   def initialize(name)
     @name = name
-    @account_no = @@number_of_accounts += 1
+    @account_no = @@number_of_objects += 1
     @balance = 1000
   end
 
   def deposit(amount)
     if amount < 0
-      'Please enter a positive amount.'
+      MESSAGE
     else
-      "Balance = #{ @balance += amount }"
+      @balance += amount
     end
   end
 
   def withdrawal(amount)
     if amount < 0
-      'Please enter a positive amount.'
+      MESSAGE
     elsif amount > @balance
       'Not enough funds'
     else
-      "Balance = #{ @balance -= amount }"
+      @balance -= amount
     end
+  end
+
+  def to_s
+    "#{ @name } has account number: #{ @account_no } with balance = #{ @balance }"
   end
 
 end
