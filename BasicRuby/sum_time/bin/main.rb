bc488bf071770b442ef829cbf41ee542783a71f5
Gaurav Mahajan <gaurav@vinsol.com>
2014-07-25 15:17:40 +0530

    Redefine valid. Use single regex.

--- BasicRuby/sum_time/bin/main.rb
+++ BasicRuby/sum_time/bin/main.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/time'
 
 puts 'Enter 1st time(hh:mm:ss):'
-time1 = gets
+time1 = gets.chomp
 puts 'Enter 2nd time(hh:mm:ss):'
-time2 = gets
+time2 = gets.chomp
 puts Time.sum(time1, time2)
--- BasicRuby/sum_time/lib/time.rb
+++ BasicRuby/sum_time/lib/time.rb
@@ -2,73 +2,57 @@ require 'time'
 
 class Time
 
-  REGEXP1 = /(?<hour1>^\d\d?):(?<min1>\d\d?):(?<sec1>\d\d?)/
-  REGEXP2 = /(?<hour2>^\d\d?):(?<min2>\d\d?):(?<sec2>\d\d?)/
+  REGEXP = /^([0-1]?\d|2[0-3]):([0-5]?\d):([0-5]?\d)$/
 
-  def self.valid?(time1, time2)
-    if time1 !~ REGEXP1
-      $check = 1
-      false
-    elsif time2 !~ REGEXP2
-      $check = 2
-      false
-    else
-      /(?<hour1>^\d\d?):(?<min1>\d\d?):(?<sec1>\d\d?)/ =~ time1
-      /(?<hour2>^\d\d?):(?<min2>\d\d?):(?<sec2>\d\d?)/ =~ time2
-      if (hour1.to_i >= 24 || min1.to_i >= 60 || sec1.to_i >= 60)
-        $check = 1
-        false
-      elsif (hour2.to_i >= 24 || min2.to_i >= 60 || sec2.to_i >= 60)
-        $check = 2
-        false
-      else
-        true
-      end
-    end
-  end
-
-  def self.sum_of_times(time1, time2)
-    $sum_seconds = time1.sec + time2.sec
-    $sum_minutes = time1.min + time2.min
-    $sum_hours = time1.hour + time2.hour
+  def self.valid?(time)
+    time =~ REGEXP
   end
 
   def self.normalise
-    if $sum_seconds >= 60
-      $sum_seconds -= 60
-      $sum_minutes +=1
+    if @@sum_seconds >= 60
+      @@sum_seconds -= 60
+      @@sum_minutes +=1
     end
 
-    if $sum_minutes >= 60
-      $sum_minutes -= 60
-      $sum_hours +=1
+    if @@sum_minutes >= 60
+      @@sum_minutes -= 60
+      @@sum_hours +=1
     end
 
-    if $sum_hours >= 24
-      $sum_hours -= 24
-      $days += 1
+    if @@sum_hours >= 24
+      @@sum_hours -= 24
+      @@days += 1
+    end
+  end
+
+  def self.show_error(time)
+    "#{ time } is not valid."
+  end
+
+  def self.show_sum
+    if @@days == 1
+      "1 day and #{ @@sum_hours }:#{ @@sum_minutes }:#{ @@sum_seconds }"
+    else
+      "#{ @@sum_hours }:#{ @@sum_minutes }:#{ @@sum_seconds }"
     end
   end
 
   def self.sum(time1, time2)
-    $days = 0
-    $check
-    if valid?(time1, time2)
-      time1 = Time.parse(time1)
-      time2 = Time.parse(time2)
-      sum_of_times(time1, time2)
-      normalise
-      if $days == 1
-        "1 day and #{ $sum_hours }:#{ $sum_minutes }:#{ $sum_seconds }"
+    @@days = 0
+    if valid?(time1)
+      if valid?(time2)
+        time1 = Time.parse(time1)
+        time2 = Time.parse(time2)
+        @@sum_seconds = time1.sec + time2.sec
+        @@sum_minutes = time1.min + time2.min
+        @@sum_hours = time1.hour + time2.hour
+        normalise
+        show_sum
       else
-        "#{ $sum_hours }:#{ $sum_minutes }:#{ $sum_seconds }"
+        show_error(time2)
       end
     else
-      if $check == 1
-        "#{ time1 } is not valid"
-      else
-        "#{ time2 } is not valid"
-      end
+      show_error(time1)
     end
   end
 
