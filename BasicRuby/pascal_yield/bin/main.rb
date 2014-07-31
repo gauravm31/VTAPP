06c063b02a364a9f6d0e0558ea09a0b43300ba45
Gaurav Mahajan <gaurav@vinsol.com>
2014-07-31 13:09:01 +0530

    Optimise Pascal.build method. Remove trailing spaces from output.

--- BasicRuby/pascal_yield/bin/main.rb
+++ BasicRuby/pascal_yield/bin/main.rb
@@ -1,6 +1,5 @@
 require_relative '../lib/pascal'
 
 Pascal.build(6) do |row|
-  row.each { |element| print "#{ element } " }
-  print "\n"
+  puts row.join(" ")
 end
--- BasicRuby/pascal_yield/lib/pascal.rb
+++ BasicRuby/pascal_yield/lib/pascal.rb
@@ -3,13 +3,12 @@ class Pascal
   def self.build(number)
     array = []
     0.upto(number) do |row|
-      nested_array = []
-      nested_array[0] = 1
+      nested_array = [1]
       1.upto(row) do |column|
-        nested_array[column] = array[row - 1][column - 1] + (array[row - 1][column] || 0)
+        nested_array[column] = array[column - 1] + (array[column] || 0)
       end
       yield(nested_array)
-      array[row] = nested_array
+      array = nested_array
     end
   end
 
