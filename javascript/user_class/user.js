e13635bd5be8ddd40a59921d730d0bfd31b17b89
Gaurav Mahajan <gaurav@vinsol.com>
2014-08-13 15:28:59 +0530

    add files for user_class exercise in javscript

---
+++ javascript/user_class/user.js
@@ -0,0 +1,19 @@
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+  this.compare = function(anotherUser) {
+    if(this.age > anotherUser.age){
+    alert(this.name + " is older than " + anotherUser.name);
+    } else if (this.age < anotherUser.age) {
+    alert(anotherUser.name + " is older than " + this.name);
+    } else {
+    alert("Both are of same age");
+    }
+  }
+};
+
+window.onload = function() {
+  user1 = new User("Mary", 23);
+  user2 = new User("John", 22);
+  user1.compare(user2);
+}
