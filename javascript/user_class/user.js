404d44b983d9f221da27ff9e8b410f0a674f7b54
Gaurav Mahajan <gaurav@vinsol.com>
2014-08-19 11:55:53 +0530

    Use prototyping and remove indentation issues.

--- javascript/user_class/user.js
+++ javascript/user_class/user.js
@@ -1,14 +1,15 @@
 function User(name, age) {
   this.name = name;
   this.age = age;
-  this.compare = function(anotherUser) {
-    if(this.age > anotherUser.age){
+};
+
+User.prototype.compare = function(anotherUser) {
+  if(this.age > anotherUser.age) {
     alert(this.name + " is older than " + anotherUser.name);
-    } else if (this.age < anotherUser.age) {
+  } else if (this.age < anotherUser.age) {
     alert(anotherUser.name + " is older than " + this.name);
-    } else {
+  } else {
     alert("Both are of same age");
-    }
   }
 };
 
