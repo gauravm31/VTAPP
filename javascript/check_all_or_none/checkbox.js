2a3f9d361597e3d0645b27423c5e95561b611a11
Gaurav Mahajan <gaurav@vinsol.com>
2014-08-19 15:43:43 +0530

    Use DRY principal. Avoid running variable assignment every time on method call.

--- javascript/check_all_or_none/checkbox.js
+++ javascript/check_all_or_none/checkbox.js
@@ -1,27 +1,24 @@
 function Checkbox() {
 };
 
-Checkbox.prototype.checkAll = function() {
-  event.preventDefault();
-  var checkboxes = document.getElementsByName("color");
-  for (var i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].checked = "checked";
-  }
-};
+var checkboxes = document.getElementsByName("color");
 
-Checkbox.prototype.checkNone = function() {
-  event.preventDefault();
-  var checkboxes = document.getElementsByName("color");
+Checkbox.prototype.check = function() {
   for (var i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].checked = "";
+    if(this.id === "check_all") {
+      checkboxes[i].checked = "checked";
+    } else {
+      checkboxes[i].checked = "";
+    }
   }
+  event.preventDefault();
 };
 
 Checkbox.prototype.bindEvents = function() {
   var checkAllLink = document.getElementById('check_all');
-  checkAllLink.addEventListener("click", this.checkAll);
+  checkAllLink.addEventListener("click", this.check);
   var checkNoneLink = document.getElementById('check_none');
-  checkNoneLink.addEventListener("click", this.checkNone);
+  checkNoneLink.addEventListener("click", this.check);
 };
 
 window.onload = function() {
