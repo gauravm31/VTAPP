d2386daad630a6b0c1eb60ee9981cc97e787e655
Gaurav Mahajan <gaurav@vinsol.com>
2014-08-20 10:47:50 +0530

    Add files for form_validation exercise

---
+++ javascript/form_validation/form.js
@@ -0,0 +1,41 @@
+function Form() {
+
+};
+
+var inputElements = document.getElementsByTagName("input");
+var textArea = document.getElementById("about_me");
+var checkBox = document.getElementById("notification");
+
+Form.prototype.validate = function() {
+  var check = true;
+  for (var i = 0, len = inputElements.length; i < len; i++) {
+    if((inputElements[i].getAttribute("type") == "text") && (inputElements[i].value === "")) {
+      alert(inputElements[i].id + " can't be empty.");
+      check = false;
+    }
+  }
+
+  if(textArea.value.length < 50) {
+    alert("Your description about yourself is too short.");
+    check = false;
+  }
+
+  if(!checkBox.checked) {
+    alert("Please check the receive notification checkbox.");
+    check = false;
+  }
+
+  if(check == false){
+    event.preventDefault();
+  }
+}
+
+Form.prototype.bindEvents = function() {
+  var form1 = document.forms[0];
+  form1.addEventListener("submit", this.validate);
+}
+
+window.onload = function() {
+  var form = new Form();
+  form.bindEvents();
+};
