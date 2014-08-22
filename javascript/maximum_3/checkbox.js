0ea4a5e1a04eecee99eda9fa9f21b2c1b01fd9c2
Gaurav Mahajan <gaurav@vinsol.com>
2014-08-22 15:07:49 +0530

    modify function to support expansion. Use true/false instead of empty string

--- javascript/maximum_3/checkbox.js
+++ javascript/maximum_3/checkbox.js
@@ -1,45 +1,54 @@
-function Checkbox() {
+function CheckboxGroup(name , id, limit) {
+  this.boxesChecked = 0;
+  this.checkBoxLimit = limit;
+  this.checkboxes = document.getElementsByName(name);
+  this.noneCheckbox = document.getElementById(id);
 };
 
-var boxesChecked = 0;
-var checkboxes = document.getElementsByName("weekday");
-var noneCheckbox = document.getElementById("none");
 
-Checkbox.prototype.checkNone = function() {
-  boxesChecked = 0;
-  for (var i = 0; (i < checkboxes.length - 1); i++) {
-    checkboxes[i].checked = "";
+
+CheckboxGroup.prototype.checkNone = function() {
+  this.boxesChecked = 0;
+  for (var i = 0; (i < this.checkboxes.length - 1); i++) {
+    this.checkboxes[i].checked = false;
   }
 };
 
-Checkbox.prototype.checkMax = function() {
-  if(this.checked) {
-    if(boxesChecked == 3) {
+CheckboxGroup.prototype.checkMax = function(obj) {
+  if(obj.checked) {
+    if(this.boxesChecked == this.checkBoxLimit) {
       event.preventDefault();
       var msg = ""
-      for (var i = 0; (i < checkboxes.length - 1); i++) {
-        if(checkboxes[i].checked && checkboxes[i]!==this) {
-          msg += ' ' + checkboxes[i].value;
+      for (var i = 0; (i < this.checkboxes.length - 1); i++) {
+        if(this.checkboxes[i].checked && this.checkboxes[i]!==obj) {
+          msg += ' ' + this.checkboxes[i].value;
         }
       }
       alert('Only three days can be seleccted. You have already selected' + msg);
     } else {
-      noneCheckbox.checked = "";
-      boxesChecked += 1;
+      this.noneCheckbox.checked = false;
+      this.boxesChecked += 1;
     }
   } else {
-    boxesChecked -=1;
+    this.boxesChecked -=1;
   }
 };
 
-Checkbox.prototype.bindEvents = function() {
-  noneCheckbox.addEventListener("click", this.checkNone);
-  for (var i = 0; (i < checkboxes.length - 1); i++) {
-    checkboxes[i].addEventListener("click", this.checkMax);
+CheckboxGroup.prototype.bindEvents = function() {
+  var _this = this;
+  this.noneCheckbox.addEventListener("click", function() {
+    _this.checkNone();
+  }
+  );
+  for (var i = 0; (i < this.checkboxes.length - 1); i++) {
+    this.checkboxes[i].addEventListener("click", function() {
+      _this.checkMax(this);
+    }
+    );
   }
 };
 
 window.onload = function() {
-  var checkbox = new Checkbox();
-  checkbox.bindEvents();
+  var weekdays = new CheckboxGroup("weekday", "none", 3);
+  weekdays.bindEvents();
 };
