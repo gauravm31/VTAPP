d4fb0683a9bd264da4fb13aa7752b23a60dad8a1
Gaurav Mahajan <gaurav@vinsol.com>
2014-08-22 15:33:35 +0530

    change function name from 'url.promptForUrl' to 'url.prompt', Remove function init as it is not required. Optimise code

--- javascript/input_url/index.js
+++ javascript/input_url/index.js
@@ -1,25 +1,24 @@
 var BLANK = /^\s*$/;
 var CHECK = /^https:\/\//;
 function Url() {
-  this.url = ""
 }
 
-Url.prototype.init = function() {
-  this.promptForUrl();
-};
-
-Url.prototype.promptForUrl = function() {
-  this.url = prompt("Enter the url", "");
+Url.prototype.prompt = function() {
+  this.url = prompt("Enter the url");
   if(BLANK.test(this.url)) {
     alert("empty URL");
   } else if(CHECK.test(this.url)) {
-    window.open(this.url, "_blank", "width=400, height=450, menubar=no, scrollbars=no, status=no, titlebar=no, toolbar=no", "false");
+    this.openNewWindow(this.url)
   } else {
-    window.open('https://' + this.url, "_blank", "width=400, height=450, menubar=no, scrollbars=no, status=no, titlebar=no, toolbar=no", "false");
+    this.openNewWindow('https://' + this.url)
   }
 };
 
+Url.prototype.openNewWindow = function(address) {
+  window.open(address, "_blank", "width=400, height=450, menubar=no, scrollbars=no, status=no, titlebar=no, toolbar=no", "false");
+}
+
 window.onload = function() {
   var link = new Url();
-  link.init();
+  link.prompt();
 }
