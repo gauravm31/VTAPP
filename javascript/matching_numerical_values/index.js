e8e34e6b876c5e634f0c6ffa9a178514e1e283c3
Gaurav Mahajan <gaurav@vinsol.com>
2014-08-23 14:40:26 +0530

    Use dom elements as object properties. Take preventDefault out of conditions.

--- javascript/matching_numerical_values/index.js
+++ javascript/matching_numerical_values/index.js
@@ -1,27 +1,29 @@
 REGEX = /^\s*-?\d*\.?\d+\s*$/
 
 var input = {
+  number: null,
+  result: null,
+  form: null,
   check: function() {
-    var number = document.getElementById("number");
-    var result = document.getElementById("result");
-    if(REGEX.test(number.value)) {
-      result.value = true;
-      event.preventDefault();
+    event.preventDefault();
+    if(REGEX.test(this.number.value)) {
+      this.result.value = true;
       setTimeout(function() {
         document.forms[0].submit();
       }, 1000);
     } else {
-      result.value = false;
-      event.preventDefault();
+      this.result.value = false;
     }
   },
 
   bindevents: function() {
-    var form = document.forms[0];
-    form.addEventListener("submit", this.check);
+    this.form.addEventListener("submit", this.check);
   }
 }
 
 window.onload = function() {
+  input.number = document.getElementById("number");
+  input.result = document.getElementById("result");
+  input.form = document.forms[0];
   input.bindevents();
 }
