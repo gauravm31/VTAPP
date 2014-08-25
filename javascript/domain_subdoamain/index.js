9285445052b5e6f9455a20116237d0ba6e77f8d9
Gaurav Mahajan <gaurav@vinsol.com>
2014-08-25 14:34:07 +0530

    Avoid repeated code. Use prototype method.

--- javascript/domain_subdoamain/index.js
+++ javascript/domain_subdoamain/index.js
@@ -1,27 +1,41 @@
 REGEX = /^(?:http(?:s)?:\/\/)?(?:www\.)?((?:(?:[a-z0-9_\-~]+)\.)*)((?:[a-z0-9_\-~]+)\.[a-z]{3,6})(\/[a-z0-9#-_~]*)*$/i;
 
-var Input = {
-  findDomain: function() {
-    var url = document.getElementById("url").value;
-    var match = REGEX.exec(url);
-    if(match) {
-      if(match[1]) {
-        alert(' domain: ' + match[2] + ', ' + 'subdomain: ' + match[1].substring(0, match[1].length - 1));
-      } else {
-        alert('domain: ' + match[2]);
-      }
-      event.preventDefault();
-    } else {
-      alert("invalid url");
-      event.preventDefault();
-    }
-  },
-  bindEvents: function() {
-    var form = document.forms[0];
-    form.addEventListener("submit", this.findDomain);
+function Input() {
+  this.domain = '';
+  this.subdomain = '';
+}
+
+Input.prototype.findDomainAndSubdomain = function() {
+  var url = document.getElementById("url").value;
+  var match = REGEX.exec(url);
+  if(match) {
+    this.domain = match[2];
+    this.subdomain = match[1]
+  } else {
+    alert("invalid url");
+    event.preventDefault();
   }
 }
 
+Input.prototype.showDomainAndSubdomain = function() {
+  if(this.subdomain !== '') {
+    alert(' domain: ' + this.domain + ', ' + 'subdomain: ' + this.subdomain.substring(0, this.subdomain.length - 1));
+  } else if(this.domain !== '') {
+    alert('domain: ' + this.domain);
+  }
+}
+
+Input.prototype.bindEvents = function() {
+  var form = document.forms[0];
+  _this = this;
+  form.addEventListener("submit", function() {
+    _this.findDomainAndSubdomain();
+    _this.showDomainAndSubdomain();
+  }
+  );
+}
+
 window.onload = function() {
-  Input.bindEvents();
+  var input = new Input();
+  input.bindEvents();
 }
