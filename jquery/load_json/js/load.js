03c5ec5a5fa5eb7ee4d3f43ffc8a830d954099ae
Gaurav Mahajan <gaurav@vinsol.com>
2014-08-27 21:17:06 +0530

    Add files for 'load_json' exercise.

---
+++ jquery/load_json/js/load.js
@@ -0,0 +1,46 @@
+function Loader() {
+  this.$select = $("select");
+  this.$form = $("#specials form");
+  this.$targetDiv = $("<div></div>");
+  this.data = null;
+}
+
+Loader.prototype.createTargetDiv = function() {
+    this.$form.after(this.$targetDiv);
+}
+
+Loader.prototype.getJson = function() {
+  var _this = this;
+  this.json = $.ajax({
+    url: 'data/specials.json',
+    dataType: 'json',
+    success: function(data) {_this.data = data},
+    error: function() {console.log("failure");}
+  })
+}
+
+Loader.prototype.start = function(obj) {
+  var specialData = this.data[$(obj).val()],
+      title = specialData['title']
+      text = specialData['text'],
+      imageSource = specialData['image'].substring(1, specialData['image'].length),
+      color = specialData['color'];
+  this.$targetDiv.empty();
+  this.$targetDiv.css("color", color);
+  this.$targetDiv.append('<h2>' + title + '</h2>' + text + '<br>')
+  this.$targetDiv.append("<img src='" + imageSource + "'>");
+}
+
+Loader.prototype.bindEvents = function() {
+  _this = this;
+  this.$select.on("change", function(){
+    _this.start(this);
+  });
+}
+
+$(function() {
+  var loader = new Loader();
+  loader.createTargetDiv();
+  loader.getJson();
+  loader.bindEvents();
+})
