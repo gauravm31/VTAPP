b218d59ca4ef7e635558230dca9810ab77416f8a
Gaurav Mahajan <gaurav@vinsol.com>
2014-08-27 18:34:26 +0530

    Add files for 'laod_external_content' exercise.

---
+++ jquery/load_external_content/js/load.js
@@ -0,0 +1,30 @@
+function Loader() {
+  this.$module = $("div#blog");
+}
+
+Loader.prototype.createTargetDiv = function() {
+  var headings = this.$module.find("h3");
+  $(headings).each(function() {
+    var $targetDiv = $("<div></div>");
+    $(this).after($targetDiv);
+    $(this).data("targetDiv", $targetDiv);
+  })
+}
+
+Loader.prototype.start = function(event) {
+  debugger
+  var id = $(this).attr('href').split("#")[1];
+  $(this).parent().data('targetDiv').load('data/blog.html #' + id);
+  event.preventDefault();  
+}
+
+Loader.prototype.bindEvents = function() {
+  _this = this;
+  this.$module.on("click", "a", this.start);
+}
+
+$(function() {
+  var loader = new Loader();
+  loader.createTargetDiv();
+  loader.bindEvents();
+})
