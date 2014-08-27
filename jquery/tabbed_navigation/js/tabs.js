d26c4f6196087442af17dda675b7ce67c1a93f57
Gaurav Mahajan <gaurav@vinsol.com>
2014-08-27 17:10:55 +0530

    Reduce DOM adding.

--- jquery/tabbed_navigation/js/tabs.js
+++ jquery/tabbed_navigation/js/tabs.js
@@ -1,34 +1,35 @@
 function Elements() {
-  this.divs = $("div.module");
-  this.unorderedList = $("<ul></ul>");
+  this.$divs = $("div.module");
+  this.$unorderedList = $("<ul></ul>");
 }
 
 Elements.prototype.performRequiredActions = function() {
-  this.divs.hide();
+  this.$divs.hide();
   this.createList();
-  this.divs.first().show().before(this.unorderedList);
+  this.$divs.first().show().before(this.$unorderedList);
 }
 
 Elements.prototype.createList = function() {
-  var _this = this;
-  this.divs.each(function() {
-    var li = $("<li></li>");
-    li.text($(this).find("h2").text());
-      _this.unorderedList.append(li);
+  var _this = this,
+      li = [];
+  this.$divs.each(function() {
+    li[li.length] = $("<li>");
+    li[li.length -1].text($(this).find("h2").text());
   })
+  _this.$unorderedList.append(li);
 }
 
 Elements.prototype.showRelatedModule = function(obj) {
   var div = $("div#" + $(obj).text().toLowerCase());
-  this.divs.hide();
+  this.$divs.hide();
   div.show();
-  this.unorderedList.children().removeClass("current")
+  this.$unorderedList.children().removeClass("current")
   $(obj).addClass("current");
 }
 
 Elements.prototype.bindEvents = function() {
   _this = this;
-  this.unorderedList.on('click', 'li', function() {
+  this.$unorderedList.on('click', 'li', function() {
     _this.showRelatedModule(this);
   })
 }
