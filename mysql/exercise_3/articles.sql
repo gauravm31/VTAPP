ee37bc8c596b3912a2917d0dbc0c00e2b82f6ab7
Gaurav Mahajan <gaurav@vinsol.com>
2014-09-09 12:36:53 +0530

    Add files for exercise_3.

---
+++ mysql/exercise_3/articles.sql
@@ -0,0 +1,104 @@
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(20) NOT NULL,
+  type SET('admin', 'normal'),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE articles (
+  id int NOT NULL AUTO_INCREMENT,
+  user_id int,
+  category varchar(20),
+  content text,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE comments (
+  id int NOT NULL AUTO_INCREMENT,
+  article_id int,
+  user_id int,
+  content text,
+  PRIMARY KEY (id)
+);
+
+-- Populate users.
+INSERT INTO users VALUES (NULL, 'User1', 'admin');
+INSERT INTO users VALUES (NULL, 'User2', 'normal');
+INSERT INTO users VALUES (NULL, 'User3', 'normal');
+INSERT INTO users VALUES (NULL, 'User4', 'normal');
+
+-- Populate articles
+INSERT INTO articles VALUES (NULL, 2, 'sci-fi', 'Article 1');
+INSERT INTO articles VALUES (NULL, 3, 'ruby', 'Article 2');
+INSERT INTO articles VALUES (NULL, 3, 'sql', 'Article 3');
+INSERT INTO articles VALUES (NULL, 1, 'ruby', 'Article 4');
+
+--Populate comments
+INSERT INTO comments VALUES (NULL, 1, 2, 'Comment 1');
+INSERT INTO comments VALUES (NULL, 2, 4, 'Comment 2');
+INSERT INTO comments VALUES (NULL, 2, 2, 'Comment 3');
+INSERT INTO comments VALUES (NULL, 4, 1, 'Comment 4');
+
+--select all articles whose author's name is user3(without using variable)
+SELECT articles.content
+FROM articles
+INNER JOIN users ON (users.id = articles.user_id)
+WHERE users.name = 'User3';
+
+-- select all articles whose author's name is user3(using variable)
+SET @user_number = 'User3';
+SELECT articles.content
+FROM articles
+INNER JOIN users ON (users.id = articles.user_id)
+WHERE users.name = @user_number;
+
+--select all the articles and also the comments associated with those articles in a single query (without using subquery)
+SELECT articles.content AS article, comments.content AS comment
+FROM articles
+INNER JOIN comments ON (comments.article_id = articles.id)
+INNER JOIN users ON (users.id = articles.user_id)
+WHERE users.name = 'User3';
+
+--select all the articles and also the comments associated with those articles in a single query (using subquery)
+
+SELECT articles.content AS article, comments.content AS comment
+FROM articles
+INNER JOIN comments ON (comments.article_id = articles.id)
+WHERE articles.content IN (
+  SELECT articles.content
+  FROM articles
+  INNER JOIN users ON (users.id = articles.user_id)
+  WHERE users.name = 'User3'
+);
+
+--select all articles which do not have any comments(without subqueries)
+SELECT articles.content AS article
+FROM articles
+LEFT JOIN comments ON (comments.article_id = articles.id)
+GROUP BY articles.content
+HAVING COUNT(comments.content) = 0;
+
+----select all articles which do not have any comments(with subqueries)
+SELECT articles.content
+FROM articles
+WHERE articles.id not IN (
+  SELECT DISTINCT comments.article_id 
+  FROM comments
+);
+
+
+--select article which has most comments
+SELECT articles.content AS article
+FROM articles
+INNER JOIN comments ON (comments.article_id = articles.id)
+GROUP BY articles.content
+ORDER BY (COUNT(comments.content)) DESC
+LIMIT 1;
+
+--select article which does not have more than one comment by the same user
+SELECT articles.content AS article
+FROM articles
+LEFT JOIN comments ON (comments.article_id = articles.id)
+LEFT JOIN users ON (comments.user_id = users.id)
+GROUP BY article
+HAVING count(distinct comments.user_id) = count(comments.user_id);
---
+++ mysql/exercise_3/dump.sql
@@ -0,0 +1,104 @@
+-- MySQL dump 10.13  Distrib 5.6.19, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: social_network
+-- ------------------------------------------------------
+-- Server version	5.6.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `category` varchar(20) DEFAULT NULL,
+  `content` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `articles`
+--
+
+LOCK TABLES `articles` WRITE;
+/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
+INSERT INTO `articles` VALUES (1,2,'sci-fi','Article 1'),(2,3,'ruby','Article 2'),(3,3,'sql','Article 3'),(4,1,'ruby','Article 4');
+/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `content` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (1,1,2,'Comment 1'),(2,2,4,'Comment 2'),(3,2,2,'Comment 3'),(4,4,1,'Comment 4'),(5,2,2,'comment 5');
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `type` set('admin','normal') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'User1','admin'),(2,'User2','normal'),(3,'User3','normal'),(4,'User4','normal');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-09-09 12:21:15
