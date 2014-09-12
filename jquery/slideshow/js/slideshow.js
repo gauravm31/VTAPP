2517e163b4c20acc47793f1860aa1a9cdddf59d9
Gaurav Mahajan <gaurav@vinsol.com>
2014-09-12 20:11:53 +0530

    Pass jquery object directly to slideshow constructor

--- jquery/slideshow/js/slideshow.js
+++ jquery/slideshow/js/slideshow.js
@@ -1,18 +1,20 @@
-function Slideshow(slideshow_id, delay_interval) {
-  this.$unorderedList = $(slideshow_id);
+function Slideshow(slideshow_id, delay_interval, fade_in_interval, fade_out_interval) {
+  this.$unorderedList = slideshow_id;
   this.$listItems = this.$unorderedList.children("li");
   this.$navigationArea = $("<div>", { id: 'navigation_area' });
   this.$imageNumberSpan = $("<span>", { id: 'image_number'});
   this.delay =delay_interval;
+  this.fadeInInterval = fade_in_interval;
+  this.fadeOutInterval = fade_out_interval;
 }
 
 Slideshow.prototype.start = function(imageNumber) {
   var _this = this
   imageNumber = this.setImageNumber(imageNumber);
   this.setNavigationAreaText(imageNumber);
-  $(this.$listItems[imageNumber]).fadeIn("slow")
+  $(this.$listItems[imageNumber]).fadeIn(this.fadeInInterval)
                                   .delay(this.delay)
-                                  .fadeOut("slow", function() {
+                                  .fadeOut(this.fadeOutInterval, function() {
     _this.start(imageNumber + 1);
   });
 }
@@ -44,7 +46,7 @@ Slideshow.prototype.setNavigationArea = function() {
 }
 
 $(document).ready(function() {
-  slideshow = new Slideshow("#slideshow", 2000);
+  slideshow = new Slideshow($("#slideshow"), 2000, "slow", "slow");
   slideshow.moveToTop();
   slideshow.setNavigationArea();
   slideshow.start(0);
