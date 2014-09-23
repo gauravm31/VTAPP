4c7ee52121da524c6ba526656301f29333928f2b
Gaurav Mahajan <gaurav@vinsol.com>
2014-09-23 14:52:11 +0530

    Add another string without any singleton method

--- advance_ruby/singleton/bin/main.rb
+++ advance_ruby/singleton/bin/main.rb
@@ -1,15 +1,25 @@
 str = 'patrick'
 
-def str.show_self
-  puts self
-end
+# First way to define singleton method
+def str.palindrome?
+    reverse == self
+  end
 
-str.show_self
+p str.palindrome?
 
+# Second way to define singleton method
 class << str
-  def show_self
-    puts self
+  def count_occurences
+    count = Hash.new(0)
+    each_char { |c| count[c] += 1 }
+    count
   end
 end
 
-str.show_self
+p str.count_occurences
+
+
+# Another string with these methods not defined
+another_str = 'patrick'
+
+another_str.show_self
