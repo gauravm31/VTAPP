1406b2630c58873d0754cb19c49c94a87692bb27
Gaurav Mahajan <gaurav@vinsol.com>
2014-09-26 12:33:58 +0530

    Ask user for arguments if present.

--- advance_ruby/dynamic_methods/bin/main.rb
+++ advance_ruby/dynamic_methods/bin/main.rb
@@ -3,7 +3,13 @@ require_relative '../lib/my_string'
 puts 'Enter the object of class MyString'
 str = MyString.new(gets.chomp)
 
-puts "Enter the method: #{MyString.instance_methods(false)}"
-method = gets.chomp
+puts "Enter the method:"
+meth = gets.chomp
+arguments = ''
 
-puts str.instance_eval method
+if (MyString.instance_methods(false).include?(meth.to_sym)) && (str.method(meth.to_s).parameters.length > 0)
+  puts "arguments = #{ str.method(meth.to_s).parameters.to_s }"
+  arguments += gets.chomp
+end
+
+puts str.instance_eval(meth + arguments)
