98c3d2988132ca4744fe1696cab2f6487f58883d
Shubham Gupta <shubham@vinsol.com>
2014-09-26 16:52:48 +0900

    Move all class code to dynamic class creator

--- advance_ruby/csv_classes/bin/main.rb
+++ advance_ruby/csv_classes/bin/main.rb
@@ -1,36 +1,10 @@
 require 'csv'
+require 'debugger'
 require_relative '../lib/dynamic_class_creator'
 
-file_name = 'persons.csv'
-dynamic_class_creator = DynamicClassCreator.new(file_name)
+file_name = 'bin/persons.csv'
 
-created_class = dynamic_class_creator.create_class
-
-created_class.class_eval do
-  @self_objects = []
-  attributes = dynamic_class_creator.get_headers
-  define_method :initialize do |row|
-    attributes.each do |attr|
-      public_send(attr + '=', row[attr])
-    end
-  end
-  define_method :to_s do
-    str = ''
-    attributes.each do |attr|
-      str += attr + ": "
-      str += public_send(attr) + " "
-    end
-    str
-  end
-
-  def self.save(obj)
-    @self_objects << obj
-  end
-
-  def self.show_objects
-    puts @self_objects
-  end
-end
+created_class = DynamicClassCreator.create_class(file_name)
 
 CSV.foreach(file_name, headers: true) do |row|
   object = created_class.new(row)
--- advance_ruby/csv_classes/lib/dynamic_class_creator.rb
+++ advance_ruby/csv_classes/lib/dynamic_class_creator.rb
@@ -1,26 +1,46 @@
+require 'debugger'
 class DynamicClassCreator
 
-  def initialize(file_name)
-    @file_name = file_name
-  end
+  def self.create_class(file_name)
+    attributes = CSV.read(file_name, headers: true).headers
 
-  def create_class()
-    attributes = CSV.read(@file_name, headers: true).headers
-    Object.const_set(generate_class_name, Class.new do
-      attributes.each do |attr|
-        attr_accessor attr.to_sym
+    klass = Class.new do
+      class_variable_set('@@file_name', file_name)
+      class_variable_set('@@headers', attributes)
+      class_variable_set('@@self_objects', Array.new)
+
+      attr_accessor *class_variable_get('@@headers')
+
+      def initialize row
+        self.class.headers.each do |attr|
+          public_send(attr + '=', row[attr])
+        end
       end
-    end)
-  end
 
-  def get_headers
-    @headers ||= CSV.read(@file_name, headers: true).headers
-  end
+      def to_s
+        str = ''
+        self.class.headers.each do |attr|
+          str += attr + ": "
+          str += public_send(attr) + " "
+        end
+        str
+      end
 
-  private
+      def self.save(obj)
+        class_variable_get('@@self_objects') << obj
+      end
 
-  def generate_class_name
-    @file_name.capitalize.partition(/\.csv/)[0]
+      def self.show_objects
+        puts class_variable_get('@@self_objects')
+      end
+
+      def self.headers
+        class_variable_get('@@headers')
+      end
+
+    end
+
+    Object.const_set('Person', klass)
   end
 
 end
